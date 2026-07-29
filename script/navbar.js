392e860d6780cf52197a363fc16d12f91542ce74
VictorBFontes <victor.jose.barros.fontes@gmail.com>
2026-07-29 18:58:20 -0300

    Correção menu mobile

--- script/navbar.js
+++ script/navbar.js
@@ -1,11 +1,14 @@
 const loginButton = document.getElementById("loginButton");
+const mobileLoginButton = document.getElementById("mobileLoginButton");
 
 const username = localStorage.getItem("username");
 
 if (loginButton && username) {
 
     loginButton.textContent = "Minha conta";
+    mobileLoginButton.textContent = "Minha conta"
 
     loginButton.href = "userarea.html";
+    mobileLoginButton.href = "userarea.html";
 
 }
